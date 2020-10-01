b3e2adda35b3e5698bc86176bb1fd647a01e4b8f
Jigme Sherpa <jigmedorjesherpa@outlook.com>
2020-10-01 11:32:04 +0545

    first Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
@@ -49,10 +63,10 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
   );
 }
-
 export default Navbar;
